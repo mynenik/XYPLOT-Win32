42cab3ca0c709c8e11b835ade79294ee2be90c81
Krishna Myneni <krishna.myneni@ccreweb.org>
2020-06-29 14:51:56 -0500

    Updated Forth environment to kForth-Win32 v1.0.16-3.
     Changes to be committed:
    	modified:   bin/xypw32.exe
    	modified:   modules/ans-words.4th
    	modified:   src/ForthCompiler.cpp
    	modified:   src/ForthVM.cpp
    	modified:   src/ForthVM.h
    	modified:   src/vm32.asm
    	modified:   src/vm32.obj
    	modified:   src/vmc.c
    	modified:   src/xydlgs.dlg

--- src/ForthVM.cpp
+++ src/ForthVM.cpp
@@ -73,7 +73,7 @@ extern "C" {
 
   // global pointers exported to other modules
 
-  int* GlobalSp;      // the global stack pointer
+  int* GlobalSp;   // the global stack pointer
   byte* GlobalTp;     // the global type stack pointer
   byte* GlobalIp;     // the global instruction pointer
   int* GlobalRp;      // the global return stack pointer
@@ -407,8 +407,6 @@ int CPP_dotparen()
 {
   // stack: ( -- | display comment and advance pTIB past end of comment )
 
-  ++pTIB;
-
   while (TRUE)
     {
       while ((pTIB < (TIB + 255)) && (! (*pTIB == ')')) && *pTIB)
@@ -436,98 +434,6 @@ int CPP_dotparen()
   return 0;
 }
 //---------------------------------------------------------------
-
-int CPP_bracketsharp()
-{
-  // stack: ( -- | initialize for number conversion )
-
-  NumberCount = 0;
-  NumberBuf[255] = 0;
-  return 0;
-}
-//----------------------------------------------------------------
-
-int CPP_sharp()
-{
-  // stack: ( ud1 -- ud2 | convert one digit of ud1 )
-
-  unsigned int u1, u2, rem;
-  char ch;
-
-  L_2dup();
-  *GlobalSp-- = 1; --GlobalTp;
-  *GlobalSp-- = Base; --GlobalTp;
-  L_mstarslash();
-  u1 = *(GlobalSp + 1);  // quotient
-  u2 = *(GlobalSp + 2);
-  // quotient is on the stack; we need the remainder
-  *GlobalSp-- = Base; --GlobalTp;
-  *GlobalSp-- = 1; --GlobalTp;
-  L_mstarslash();
-  L_dminus();
-  rem = *(GlobalSp + 2);  // get the remainder
-  *(GlobalSp + 1) = u1;   // replace rem with quotient on the stack
-  *(GlobalSp + 2) = u2;
-  ch = (rem < 10) ? (rem + 48) : (rem + 55);
-  ++NumberCount;
-  NumberBuf[255 - NumberCount] = ch;
-
-  return 0;
-}
-//----------------------------------------------------------------
-
-int CPP_sharps()
-{
-  // stack: ( ud -- 0 0 | finish converting all digits of ud )
-
-  unsigned int u1, u2;
-  u1 = 1; u2 = 0;
-
-  while (u1 | u2)
-    {
-      CPP_sharp();
-      u1 = *(GlobalSp + 1);
-      u2 = *(GlobalSp + 2);
-    }
-  return 0;
-}
-//----------------------------------------------------------------
-
-int CPP_hold()
-{
-  // stack: ( n -- | insert character into number string )
-  char ch = *(++GlobalSp); ++GlobalTp;
-  ++NumberCount;
-  NumberBuf[255-NumberCount] = ch;
-  return 0;
-}
-//----------------------------------------------------------------
-
-int CPP_sign()
-{
-  // stack: ( n -- | insert sign into number string if n < 0 )
-  int n = *(++GlobalSp); ++GlobalTp;
-  if (n < 0)
-    {
-      ++NumberCount;
-      NumberBuf[255-NumberCount] = '-';
-    }
-  return 0;
-}
-//----------------------------------------------------------------
-
-int CPP_sharpbracket()
-{
-  // stack: ( 0 0 -- | complete number conversion )
-
-  L_2drop();
-  *GlobalSp-- = (int) (NumberBuf + 255 - NumberCount);
-  *GlobalTp-- = OP_ADDR;
-  *GlobalSp-- = NumberCount;
-  *GlobalTp-- = OP_IVAL;
-  return 0;
-}
-//----------------------------------------------------------------
 
 int CPP_dot ()
 {
@@ -682,9 +588,9 @@ int CPP_uddot ()
     }
   else
     {
-      CPP_bracketsharp();
-      CPP_sharps();
-      CPP_sharpbracket();
+      C_bracketsharp();
+      C_sharps();
+      C_sharpbracket();
       CPP_type();
       *pOutStream << ' ';
       pOutStream->flush();
@@ -953,11 +859,9 @@ int CPP_word ()
 {
   // stack: ( n -- ^str | parse next word in input stream )
   // n is the delimiting character and ^str is a counted string.
-
-  char delim = *(++GlobalSp); ++GlobalTp;
+  DROP
+  char delim = TOS;
   char *dp = WordBuf + 1;
-
-  if (*pTIB == ' ') ++pTIB;
 
   while (*pTIB)
     {
@@ -1130,7 +1034,6 @@ int CPP_forget ()
 {
   char token[128];
 
-  ++pTIB;
   pTIB = ExtractName (pTIB, token);
   strupr(token);
 
@@ -1231,7 +1134,7 @@ int CPP_cquote ()
   // compilation stack: ( -- | compile a counted string into the string table )
   // runtime stack: ( -- ^str | place address of counted string on stack )
 
-  char* begin_string = pTIB + 1;
+  char* begin_string = pTIB ;
   char* end_string = strchr(begin_string, '"');
   if (end_string == NULL)
     {
@@ -1244,11 +1147,8 @@ int CPP_cquote ()
   strncpy(str+1, begin_string, nc);
   str[nc+1] = '\0';
   StringTable.push_back(str);
-  byte* bp = (byte*) &str;
-
   pCurrentOps->push_back(OP_ADDR);
-  for (int i = 0; i < sizeof(byte*); i++)
-    pCurrentOps->push_back(*(bp + i));
+  OpsPushInt((long int) str);
 
   return 0;
 }
